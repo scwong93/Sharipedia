8bcf7df1d5df16833d97a99bc5da2105a8de8d7b
Simon <Simon@Simons-MacBook-Air.local>
2017-10-16 12:34:04 -0700

    Markdown completed

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ gem 'stripe'
 
 gem 'figaro', '1.0'
 
+gem 'redcarpet'
+
 group :development do
   gem 'listen', '~> 3.0.5'
 end
--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -10,4 +10,8 @@ module WikisHelper
   def authorize_delete
     current_user == @wiki.user || current_user.admin?
   end
+
+  def markdown(body)
+    RedCarpet::Markdown.new(RedCarpet::Render::HTML, tables: true, autolink: true, underline: true)
+  end
 end
